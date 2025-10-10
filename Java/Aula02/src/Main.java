066fc7db888e566d79c83c4a16d20166f4e4dd6c
Rodrigosan003 <r.santanaa003@gmail.com>
2025-10-10 12:19:01 -0300

    Aulas de java

---
+++ Java/Aula02/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Pun: Maverick");
+
+        int anoDoLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDoLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+    }
+}
