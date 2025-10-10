b2f7e78a7b4edbc8d780d805702acee6402c1c11
Rodrigosan003 <r.santanaa003@gmail.com>
2025-10-10 14:27:51 -0300

    Aulas de java

--- Java/Aula02/src/Main.java
+++ Java/Aula02/src/Main.java
@@ -7,5 +7,19 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDoLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) /3; // Média calculada diretamente na atribuição
+        // Comentário de uma linha.
+        /* Comentário
+           de múltiplas
+           linhas.
+         */
+        System.out.println("Média dos filmes: " + media);
+        String sinopse;
+        sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println("Sinopse: " + sinopse);
+
+        int classificacao = (int) (media / 2); // Casting de double para int
+        System.out.println("Classificação indicativa: " + classificacao + " estrelas.");
     }
 }
